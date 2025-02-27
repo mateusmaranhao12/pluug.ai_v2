f1ede73c3ceef793d391ea0c88d1059469974cf8
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-02-26 22:48:20 -0300

    Implement menu dropdown

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const centralAgentesLink = document.querySelector('.central-agentes-link');
-    const centralAgentesItem = document.querySelector('.central-agentes-item');
+    const centralAgentesLink = document.querySelector('.central-agentes-link')
+    const centralAgentesItem = document.querySelector('.central-agentes-item')
 
     centralAgentesLink.addEventListener('click', () => {
-        centralAgentesItem.classList.toggle('open');
-    });
-});
+        centralAgentesItem.classList.toggle('open')
+    })
+})
+
+//menu dropdown
+function toggleMenu() {
+    const menu = document.getElementById('menuDropdown')
+    menu.classList.toggle('active')
+}
+
+// Fecha o menu ao clicar fora dele
+window.onclick = function(event) {
+    const menu = document.getElementById('menuDropdown')
+    if (!event.target.matches('.fa-chevron-down') && !event.target.closest('.menu-dropdown')) {
+        menu.classList.remove('active')
+    }
+}
